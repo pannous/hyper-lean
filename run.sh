7ef3c8a9107005fafecc1b1e42618fd80f891d44
pannous <info@pannous.com>
2026-08-26 14:13:57 +0200

    fix: run.sh - hyper.lean isn't a lake target, use lake env lean instead

--- run.sh
+++ run.sh
@@ -4,8 +4,10 @@
 
 set -e
 
+cd ~/dev/script/lean4/hyper/
+
 cd "$(dirname "$0")"
 echo HyperReal definitions and lemmas via Hyper.HyperBasics
 lake build Hyper.HyperBasics
 echo some examples in hyper.lean
-lake build hyper 
+lake env lean hyper.lean
